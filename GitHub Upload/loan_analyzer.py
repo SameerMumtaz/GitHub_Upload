bced7b3ec6ddb95ccb7bac39d6cbd96d928c0b55
SameerMumtaz <109926771+SameerMumtaz@users.noreply.github.com>
2022-08-03 23:16:35 -0500

    Delete loan_analyzer.py

--- GitHub Upload/loan_analyzer.py
+++
@@ -1,5 +0,0 @@
-loan_costs = [500, 600, 200, 1000, 450]
-number_of_loans = len(loan_costs)
-total_value_of_loans = sum(loan_costs)
-average_loan_price = (total_value_of_loans/number_of_loans)
-ls(average_loan_price)
